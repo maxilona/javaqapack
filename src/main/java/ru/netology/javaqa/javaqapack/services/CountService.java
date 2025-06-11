01aba927d1380739d52685237dd9d15242ceb025
mgvrez <maxilona2024@gmail.com>
2025-06-12 00:43:21 +0300

    -A

--- src/main/java/ru/netology/javaqa/javaqapack/services/CountService.java
+++ src/main/java/ru/netology/javaqa/javaqapack/services/CountService.java
@@ -1,4 +1,21 @@
 package ru.netology.javaqa.javaqapack.services;
 
 public class CountService {
+
+    public int calculate(int income, int expenses, int threshold) {
+
+        int count = 0; // счётчик месяцев отдыха
+        int money = 0; // количество денег на счету
+        for (int month = 0; month < 12; month++) {
+            if (money >= threshold) { // можем ли отдыхать?
+                money = money - expenses;
+                money = money / 3;
+                count ++;
+            } else {
+                money = money + income;
+            }
+        }
+        return count;
+    }
 }
+
--- src/test/java/ru/netology/javaqa/javaqapack/services/CountServiceTest.java
+++ src/test/java/ru/netology/javaqa/javaqapack/services/CountServiceTest.java
@@ -1,4 +1,25 @@
 package ru.netology.javaqa.javaqapack.services;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class CountServiceTest {
+    @Test
+    public void testCalcMonths1() {
+        CountServiceTest service = new CountServiceTest();
+        int income = 10_000;
+        int expenses = 3_000;
+        int threshold = 20_000;
+
+        int actual = service.calculate(income, expenses, threshold);
+
+        int expected = 3;
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
+
+
+
+
+
